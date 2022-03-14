b31a4cc02abf13bdb8961d33061768b32534ecf5
riccardo-fedrizzi <riccardo.fedrizzi@marconirovereto.it>
2022-03-14 10:00:25 +0000

    Added more information based on the selected country, improved UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import UserInput from "./UserInput.js";
+import './style.css'
 
 function App() {
     return (
         <div className="App">
-            <header>Covid-19 Tracker</header>
             <UserInput></UserInput>
         </div>
     );
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,24 +1,24 @@
-import { func } from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import urls from "./urls.json";
+import Chart from 'chart.js';
 
 function UserInput() {
 
     function DisplayData(props) {
         let { data } = props;
         return (
-            <div>
+            <div className="container-general">
                 <div id="container-latest">
-                    <div id="container-latest-confirmed">
+                    <div className="latest" id="container-latest-confirmed">
                         <p>Confirmed</p>
                         <p>{data.latest.confirmed}</p>
                     </div>
-                    <div id="container-latest-recovered">
+                    <div className="latest" id="container-latest-recovered">
                         <p>Recovered</p>
                         <p>{data.latest.recovered}</p>
                     </div>
-                    <div id="container-latest-deaths">
+                    <div className="latest" id="container-latest-deaths">
                         <p>Deaths</p>
                         <p>{data.latest.deaths}</p>
                     </div>
@@ -42,7 +42,7 @@ function UserInput() {
                                         <th>{item.latest.confirmed}</th>
                                         <th>{item.latest.recovered}</th>
                                         <th>{item.latest.deaths}</th>
-                                        <th><input id={item.id} type="button" value="More" onClick={fetchSpecificCountry}></input></th>
+                                        <th><input id={item.id} className="moreBtn" type="button" value="More" onClick={fetchSpecificCountry}></input></th>
                                     </tr>);
                             })}
                         </tbody>
@@ -56,34 +56,26 @@ function UserInput() {
         let data = props.data.location;
         return (
             <div>
-                <p>{data.country}</p>
+                <input type="button" onClick={fetchAllData} id="back-btn" value="Back"></input>
+                <p id="country-name">{data.country}</p>
                 <div id="container-latest">
-                    <div id="container-latest-confirmed">
+                    <div className="latest" id="container-latest-confirmed">
                         <p>Confirmed</p>
                         <p>{data.latest.confirmed}</p>
                     </div>
-                    <div id="container-latest-recovered">
+                    <div className="latest" id="container-latest-recovered">
                         <p>Recovered</p>
                         <p>{data.latest.recovered}</p>
                     </div>
-                    <div id="container-latest-deaths">
+                    <div className="latest" id="container-latest-deaths">
                         <p>Deaths</p>
                         <p>{data.latest.deaths}</p>
                     </div>
                 </div>
-                <div id="map-container">
-                    <div>
-                        <p>Confirmed</p>
-                        <canvas id="confirmed-chart" width="600" height="600"></canvas>
-                    </div>
-                    <div>
-                        <p>Recovered</p>
-                        <canvas id="recovered-chart" width="600" height="600"></canvas>
-                    </div>
-                    <div>
-                        <p>Deaths</p>
-                        <canvas id="deaths-chart" width="600" height="600"></canvas>
-                    </div>
+                <div id="chart-container">
+                        <canvas className="chart" id="confirmed-chart" width="600" height="600"></canvas>
+                        <canvas className="chart"id="recovered-chart" width="600" height="600"></canvas>
+                        <canvas className="chart" id="deaths-chart" width="600" height="600"></canvas>
                 </div>
             </div>
         );
@@ -92,39 +84,50 @@ function UserInput() {
     async function fetchAllData(e) {
         e.preventDefault();
         let data = await fetch(urls.v2.locations).then(res => res.json());
-        ReactDOM.render(<DisplayData data={data} />, document.getElementById('main-data'));
+        ReactDOM.render(<DisplayData data={data} />, document.getElementById('root'));
     }
 
     async function fetchSpecificCountry(e) {
         e.preventDefault();
+
+        let charts = ['confirmed-chart', 'recovered-chart', 'deaths-chart'];
+        let chartsLabel = ['Confirmed', 'Recovered', 'Deaths'];
         let url = `${urls.v2.locations}${e.target.id}`;
         let data = await fetch(url).then(res => res.json());
-        await ReactDOM.render(<DisplaySpecificCoutryData data={data} />, document.getElementById('main-data'));
-        let confirmedData = data.location.timelines.confirmed.timeline;
-        let confirmedDataKeys = Object.keys(confirmedData);
-        confirmedData = Object.values(confirmedData);
+        await ReactDOM.render(<DisplaySpecificCoutryData data={data} />, document.getElementById('root'));
+        data = data.location.timelines;
+        let dataKeys = Object.keys(data);
+        dataKeys.forEach((property, i) => {
+            let timeline = data[property].timeline;
+            let keys = Object.keys(timeline);
+            let datas = Object.values(timeline);
 
-        const confirmedChartData = {
-            type: 'line',
-            data: {
-                labels: confirmedDataKeys,
-                datasets: [{
-                    label: 'Cases',
-                    data: confirmedData,
-                    fill: false,
-                    borderColor: 'rgb(75, 192, 192)',
-                    tension: 0.1
-                }]
-            },
-        };
-        new Chart(document.getElementById('confirmed-chart'), confirmedChartData);
+            const confirmedChartData = {
+                type: 'line',
+                data: {
+                    labels: keys,
+                    datasets: [{
+                        label: chartsLabel[i],
+                        data: datas,
+                        fill: false,
+                        borderColor: 'rgb(75, 192, 192)',
+                        tension: 0.1
+                    }]
+                }
+            };
+
+            new Chart(document.getElementById(charts[i]), confirmedChartData);
+
+        });
+
     }
 
 
     return (
 
-        <div className="App">
-            <input onClick={fetchAllData} type="button" value="View last report"></input>
+        <div className="main-container">
+            <input onClick={fetchAllData} className="button" type="button" value="View last report"></input>
+            { /*
             <div>
                 <div>
                     <input type="button" value="Advanced Search"></input>
@@ -134,6 +137,9 @@ function UserInput() {
                     <input type="button" value="Search"></input>
                 </div>
             </div>
+             */}
+
+
             <div id="main-data"></div>
         </div>
     );
